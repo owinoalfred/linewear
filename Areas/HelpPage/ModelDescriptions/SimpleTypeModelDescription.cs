5bab1cd44e941f156f8690fa756b70f241d6d483
Alfred Owino <Alfred@saethre-sten.no>
2020-03-26 23:50:26 +0300

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace linewear.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="linewear.WebApiApplication" Language="C#" %>
